7cd1fc7235ac2f19a740b53d4a7cae50f6b83093
jped <jonathanped@gmail.com>
2019-10-18 12:58:30 -0400

    fixes and cleanings

--- hw_3/code/simpleScript.c
+++ hw_3/code/simpleScript.c
@@ -31,15 +31,28 @@ void openAndDup(int flags, char * file_name, int new_fd){
 		char error_message[MAX_ERROR_LENGTH];
 		snprintf(error_message, MAX_ERROR_LENGTH,"opening and duplicating %s for io redirect", file_name);
 		printError(error_message);
+		exit(1);
 	}else{
-		dup2(fd, new_fd);
+		int dup_resp = dup2(fd, new_fd);
+		if(dup_resp<0) {
+			char error_message[MAX_ERROR_LENGTH];
+			snprintf(error_message, MAX_ERROR_LENGTH, "duping file: %s to new_fd: %d", file_name, new_fd);
+			printError(error_message);
+			exit(1);
+		}
+		int close_resp = close(fd);
+		if(close_resp<0){
+		       	char error_message[MAX_ERROR_LENGTH];
+			snprintf(error_message, MAX_ERROR_LENGTH, "closing file %s", file_name);
+			printError(error_message);
+		}
 	}
 }
 
 int shell(FILE * input_file) {
 	char buffer[MAX_ARGUMENT_LENGTH];
 	int buff_loc = 0;
-	int comment_line = 0;
+	int is_comment_line = 0;
 	char letter;
 	char * command;
 	int exit_num = 0;
@@ -49,7 +62,7 @@ int shell(FILE * input_file) {
 		if (letter == '\n'){
 			buffer[buff_loc] = '\0';
 			buff_loc = 0;
-			comment_line = 0;
+			is_comment_line = 0;
 			// break up input into pieces
 			command = strtok(buffer, delim); 	
 			if (!command)
@@ -62,11 +75,11 @@ int shell(FILE * input_file) {
 				printf("%s\n",path_buffer);
 			} else if  (strncmp(command, "cd", 2) == 0) {
 				// grab the next argument
-				char * new_dir = strtok(NULL, delim);
-				if (new_dir == NULL){
-					new_dir = getenv("HOME");
+				char * dest_dir = strtok(NULL, delim);
+				if (dest_dir == NULL){
+					dest_dir = getenv("HOME");
 				}
-				int resp = chdir(new_dir);
+				int resp = chdir(dest_dir);
 				if (resp != 0)
 					printError("cd");
 			} else if  (strncmp(command, "exit", 4) == 0) {
@@ -83,37 +96,42 @@ int shell(FILE * input_file) {
 				struct rusage ru;
 				int status;
 				int argument_loc = 1;
+				int no_more_arguments_allowed = 0; 
 				switch(pid=fork()){
 					case -1: 
 						printError("trying to fork");
 						exit(1);
 					case 0:
-						// do the exec here and io redirect here in the child.
+						// do the exec in parent and io redirect in the child.
 						argument_list[0] = command;
 						while((next_argument = strtok(NULL, delim)) && argument_loc<MAX_NUM_ARGUMENT){
 							// if it is i/o redirection do the io redirect
-							if(strncmp(next_argument, "2>", 2) == 0){
-								file_name = next_argument + 2;
-								openAndDup(O_WRONLY|O_CREAT|O_TRUNC, file_name, 2);
+							// also make sure after io redirect we dont accept anymore arguments
+							if (strncmp(next_argument, "2>>", 3) == 0){
+								file_name = next_argument + 3;
+								openAndDup(O_WRONLY|O_CREAT|O_APPEND, file_name, 2);
+								no_more_arguments_allowed = 1;
 							}else if(strncmp(next_argument, ">>", 2) == 0){
 								file_name = next_argument + 2;
 								openAndDup(O_WRONLY|O_CREAT|O_APPEND, file_name, 1);
-							}else if(strncmp(next_argument, "2>>", 3) == 0){
-								file_name = next_argument + 3;
-								openAndDup(O_WRONLY|O_CREAT|O_APPEND, file_name, 2);
+								no_more_arguments_allowed = 1;
+							}else if(strncmp(next_argument, "2>", 2) == 0){
+								file_name = next_argument + 2;
+								openAndDup(O_WRONLY|O_CREAT|O_TRUNC, file_name, 2);
+								no_more_arguments_allowed = 1;
 							}else if(strncmp(next_argument, ">", 1) == 0){
 								file_name = next_argument + 1;
 								openAndDup(O_WRONLY|O_CREAT|O_TRUNC, file_name, 1);
+								no_more_arguments_allowed = 1;
 							}else if(strncmp(next_argument, "<", 1) == 0){
 								file_name = next_argument + 1;
 								openAndDup(O_RDONLY, file_name, 0);
-							}else{
+								no_more_arguments_allowed = 1;
+							}else if(!no_more_arguments_allowed){
 								argument_list[argument_loc] = next_argument;
 								argument_loc++;	
 							}
 							
-							
-
 						}
 						int resp = execvp(command, argument_list);
 						if (resp == -1){
@@ -127,41 +145,44 @@ int shell(FILE * input_file) {
 							printError("waiting for child process");
 						else{
 							// get exit code here
-							if(WIFEXITED(status)){
-								exit_num = WEXITSTATUS(status);
-							}else if(WIFSIGNALED(status)){
+							exit_num = WEXITSTATUS(status);
+							if(WIFSIGNALED(status)){
 								sig_num = WTERMSIG(status);
 							}
 							char exit_or_sig_string[MAX_EXIT_OR_SIG_STRING];
 							if (sig_num != -1) {
 								snprintf(exit_or_sig_string, MAX_EXIT_OR_SIG_STRING, "signal %d  (%s)", sig_num, strsignal(sig_num));
 								sig_num = -1;
+							}else if (exit_num == 0){
+								snprintf(exit_or_sig_string, MAX_EXIT_OR_SIG_STRING, "exited normally");
 							}else {
 								snprintf(exit_or_sig_string, MAX_EXIT_OR_SIG_STRING, "return value %d", exit_num);
 							}
 							// get real time
 							gettimeofday(&stop, NULL);
-							printf("Child process %d exited with %s\n Real:%1d.%.6ds User:%1d.%.6ds Sys:%1d.%.6ds\n", pid, exit_or_sig_string,
+							printf("Child process %d exited with %s\n Real:%1ld.%.6lds User:%1d.%.6ds Sys:%1d.%.6ds\n", pid, exit_or_sig_string,
 									       (long) stop.tv_sec - start.tv_sec, (long) stop.tv_usec - start.tv_usec, 
 									       (int) ru.ru_utime.tv_sec, (int) ru.ru_utime.tv_usec, 
 									       (int) ru.ru_stime.tv_sec, (int) ru.ru_stime.tv_usec);	
 						}						
 				}
 			}
-		}else if(!comment_line && buff_loc < MAX_ARGUMENT_LENGTH){
+		}else if(!is_comment_line && (buff_loc + 1) < MAX_ARGUMENT_LENGTH){
 			if (buff_loc == 0 && letter == '#')
-				comment_line = 1;
+				is_comment_line = 1;
 			else{
 				buffer[buff_loc] = letter;
 				buff_loc++;
 			}
 		}
 	}
+	printf("end of file read, exiting shell with exit code %d\n", exit_num); 
 	exit(exit_num);
 }
 
 int main(int argc, char ** argv) {
 	FILE * input_file = stdin;
+	// here we are dealing if we are running in scripting mode
 	if (argc > 1) {
 		input_file = fopen(argv[1], "r");
 		if (!input_file)
